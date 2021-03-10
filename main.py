1a573343ffbed4835942e157039bbec3d6ee987a
Ankit Shrestha <ankit.shrestha196@gmail.com>
2021-03-10 08:39:02 +0545

    Complete Nato Phonetic alphabet and fix error

--- main.py
+++ main.py
@@ -5,7 +5,15 @@ nato_phonetic_data = pandas.read_csv("nato_phonetic_alphabet.csv")
 data = {value.letter: value.code for (index, value) in nato_phonetic_data.iterrows()}
 
 
-user_input = input("Enter a word: ").upper()
+def generate_phonetic():
+    user_input = input("Enter a word: ").upper()
+    try:
+        phonetic_code = [data[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letter in the alphabet please")
+        generate_phonetic()
+    else:
+        print(phonetic_code)
 
-phonetic_code = [data[letter] for letter in user_input]
-print(phonetic_code)
+
+generate_phonetic()
